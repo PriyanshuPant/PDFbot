7de162081bea0dd21c35ad3dfd059273c4310f6e
AnanyaBt <148137236+AnanyaBt@users.noreply.github.com>
2024-01-04 17:01:31 +0530

    Update st.py

--- st.py
+++ st.py
@@ -10,6 +10,39 @@ from openai import OpenAIError
 
 from BOT import BOT
 from PDF_reader import PDF_reader
+from htmltempelates import BACKGROUND_TEMPLATE
+
+
+
+page_bg_img = """
+<style>
+[data-testid= "stAppViewContainer"] {
+background-image: url("https://wallpapercave.com/wp/wp7921358.jpg");
+background-size: cover;
+}
+
+[data-testid="stHeader"] {
+background-color: rgba(0,0,0,0);
+}
+
+[data-testid="stSidebar"]{
+background-image: url("https://i.pinimg.com/564x/fd/ba/64/fdba646e4210ef03a987b8253d22a7a5.jpg");
+background-size: cover;
+}
+
+[data-estid="element-Container"] {
+background-color: rgb(192,192,192);
+background-size: cover;
+}
+
+[data-testid="stContainer"] {
+    background-color: lightgray;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title("Galactico 💬")
+
 
 
 bot = BOT()
@@ -21,10 +54,12 @@ bot = BOT()
 pdf = None
 load_dotenv()
 
-st.header("AstroPDF 💬")
+
 user_question = st.text_input("Ask a question about your documents:")
 
 with st.sidebar:
+    st.write("To see the source code,click below:")
+    st.write("[Learn More >](https://github.com/AnanyaBt/PDFbot)") 
     st.subheader("Your documents")
     pdf_docs = st.file_uploader(
             "Upload your PDFs here'",
@@ -38,12 +73,46 @@ with st.sidebar:
 
 
 
+answer_container = st.container()
 
+col1, col2 = st.columns(2)
 
-if st.button("Ask"):
+if col1.button("Ask"):
     if pdf is not None:
         chunk, page_no = pdf.get_relevant_chunks(user_question)
         response = bot.answer(user_question, chunk, page_no)[0]
-        st.write(response)
+        with answer_container:
+            st.write(response)
+            st.write(page_no)
+
+if col2.button("Summary"):
+    if pdf is not None:
+        chunks = pdf.get_relevant_chunks(user_question)
+        summary = bot.summarize(chunks)
+        with answer_container:
+            st.write(summary)
+
+
+
+
+# if st.button("Ask"):
+#     if pdf is not None:
+#         chunk, page_no = pdf.get_relevant_chunks(user_question)
+#         response = bot.answer(user_question, chunk, page_no)[0]
+#         with answer_container:
+#             st.write(response)
+
+
+# if st.button("Summary"):
+#     if pdf is not None:
+#         chunks=pdf.get_relevant_chunks(user_question)
+#         summary = bot.summarize(chunks)
+#         with answer_container:
+#             st.write(summary)
+        
+   
+
+
+
 
 
